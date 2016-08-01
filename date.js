d64bf336e9e038ff42f688443b17cd93c11edad5
iamnabob@126.com <iamnabob@126.com>
2016-08-01 14:27:14 +0800

    代码结构优化

---
+++ date.js
@@ -0,0 +1,59 @@
+'use strict'
+
+var date = module.exports = {
+    today: function() {
+        return new Date().Format("yyyy-MM-dd");
+    },
+    nextDay: function() {
+        return new Date().add('d',1).Format("yyyy-MM-dd");
+    }
+};
+/** 
+ *js中更改日期  
+ * y年， m月， d日， h小时， n分钟，s秒  
+ */
+Date.prototype.add = function(part, value) {
+    value *= 1;
+    if (isNaN(value)) {
+        value = 0;
+    }
+    switch (part) {
+        case "y":
+            this.setFullYear(this.getFullYear() + value);
+            break;
+        case "m":
+            this.setMonth(this.getMonth() + value);
+            break;
+        case "d":
+            this.setDate(this.getDate() + value);
+            break;
+        case "h":
+            this.setHours(this.getHours() + value);
+            break;
+        case "n":
+            this.setMinutes(this.getMinutes() + value);
+            break;
+        case "s":
+            this.setSeconds(this.getSeconds() + value);
+            break;
+        default:
+
+    }
+    return this;
+}
+
+Date.prototype.Format = function(fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
---
+++ task.js
@@ -0,0 +1,318 @@
+'use strict'
+const http = require('http')
+const path = require('path')
+const url = require('url')
+const util = require('util')
+const superagent = require('superagent');
+const cheerio = require('cheerio');
+
+// lib
+var date = require('./date');
+
+//exports
+var Task = module.exports = {};
+
+Task.login = function(opts, callback) {
+	superagent.post(opts.url) //这是登录提交地址
+        .type("form")
+        .send({ account: opts.account }) //这肯定不是我真的用户名和密码
+        .send({ password: opts.password })
+        .send({ referer: opts.referer })
+        .end(function(err, res) {
+            if (err) throw err;
+            var cookie = res.header['set-cookie'] //从response中得到cookie
+
+            callback && callback(cookie);
+        });
+}
+
+/**
+ * 任务列表
+ * @param  {[cookie]}
+ * @return {[none]}
+ */
+Task.list = function(cookie) {
+
+    // closeMytask(cookie, '8775', '新标签页beta2.0需求讨论', '2016-07-29', '3');
+    // getMytaskById(cookie,'8775');
+    // return false;
+    superagent.get('http://mx5.maxthon.net/index.php?m=my&f=task')
+        .set("Cookie", cookie[2])
+        .end((err, response) => {
+
+            if (err) console.log(err)
+            let $ = cheerio.load(response.text)
+
+            let now = date.today();
+            console.log('now : ' + now);
+
+            $('#myTaskForm').find('tbody>tr').each((index, item) => {
+
+                let status = $(item).find('td').eq(11).text();
+                if(status === '已完成') {
+                    return true;
+                }
+
+                let taskID = $(item).find('td').eq(0).find('a').text();
+                let taskName = $(item).find('td').eq(3).find('a').text();
+                let finishDate = $(item).find('td').eq(10).text();
+                if(status === '未开始') {
+
+                    Task.start(cookie, taskID, taskName);
+                    return true;
+                }
+
+                if(now === finishDate) {
+               
+                    var finishHour = $(item).find('td').eq(7).text();
+
+                    Task.close(cookie, taskID, taskName, finishDate, finishHour);
+                    return true;
+                }
+
+                // console.log(taskID + '  ' + taskName);
+
+            });
+        });
+};
+
+/**
+ * 任务关闭
+ * @param  {[cookie]}
+ * @param  {[taskID]}
+ * @param  {[taskName]}
+ * @param  {[finishDate]}
+ * @param  {[finishHour]}
+ * @return {[none]}
+ */
+Task.close = function(cookie, taskID, taskName, finishDate, finishHour) {
+
+    let url = 'http://mx5.maxthon.net/index.php?m=task&f=finish&taskID=' + taskID + '&onlybody=yes';
+    superagent.get(url)
+        .set("Cookie", cookie[2]) //在resquest中设置得到的cookie
+        .end((err, response) => {
+
+            if (err) console.log(err)
+            let $ = cheerio.load(response.text);
+
+            var options = {
+                'url': url,
+                'cookie': cookie,
+                "field": { //其余post字段
+                    'comment': $('#comment').val(),
+                    'labels[]': '',
+                    'consumed': finishHour,
+                    'assignedTo': $('#assignedTo').val(),
+                    'finishedDate': $('#finishedDate').val()
+
+                }
+            };
+
+            postRequest(options, function(resCode) {
+
+                if (resCode == '200') {
+                    console.log('任务【' + taskID + '】' + taskName + ' 完成了!');
+                }
+            });
+        });
+}
+
+/**
+ * 任务启动
+ * @param  {[cookie]}
+ * @param  {[taskID]}
+ * @param  {[taskName]}
+ * @return {[none]}
+ */
+Task.start = function(cookie, taskID, taskName) {
+
+    let url = 'http://mx5.maxthon.net/index.php?m=task&f=start&taskID=' + taskID + '&onlybody=yes';
+    superagent.get(url)
+        .set("Cookie", cookie[2]) //在resquest中设置得到的cookie
+        .end((err, response) => {
+
+            if (err) console.log(err)
+            let $ = cheerio.load(response.text);
+
+            var options = {
+                'url': url,
+                'cookie': cookie,
+                "field": { //其余post字段
+                    'comment': $('#comment').val(),
+                    'consumed': '0',
+                    'left': $('#left').val(),
+                    'realStarted': $('#realStarted').val()
+                }
+            };
+
+            postRequest(options, function(resCode) {
+
+                if (resCode == '200') {
+                    console.log('任务【' + taskID + '】' + taskName + ' 已经正确的启动!');
+                }
+            });
+
+        });
+}
+
+/**
+ * @description 发送post请求
+ * @param  {[opts]}
+ * @param  {fn}
+ * @return {[none]}
+ */
+function postRequest(opts, fn) {
+
+    opts['file'] = '';
+    opts['filename'] = '';
+    opts['param'] = '';
+    opts['boundary'] = '---------------------------WebKitFormBoundary' + getBoundary();
+
+    let request_body = requestBody(opts);
+
+    let header_options = url.parse(opts.url);
+    header_options.headers = {
+        'Content-Length': Buffer.byteLength(request_body),
+        'Content-Type': 'multipart/form-data; boundary=' + opts.boundary,
+        'Cookie': (opts.cookie)[2],
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+        'Connection': 'keep-alive'
+    };
+    header_options.method = 'POST';
+
+    // console.log(body);
+    // return false;
+    var req = http.request(header_options, function(res) {
+        var data = '';
+        res.on('data', function(chunk) {
+            data += chunk;
+        });
+        res.on('end', function() {
+            fn && fn(res.statusCode);
+        });
+    });
+
+    req.end(request_body);
+}
+
+/**
+ * 请求body主体
+ * @param  {[opts]}
+ * @return {[type]}
+ */
+function requestBody(opts) {
+
+    let h = getBoundaryBorder(opts.boundary);
+    let e = fieldPayload(opts);
+    let a = getfieldHead(opts.param, opts.filename);
+    let d = '--' + opts.boundary + "--";
+
+    return h + e + a + d;
+}
+
+function mkfield(field, value) {
+  return util.format('Content-Disposition: form-data; name="%s"\r\n\r\n%s', field, value);
+}
+//post值payload
+function getfield(field, value) {
+    return 'Content-Disposition: form-data; name="' + field + '"\r\n\r\n' + value + '\r\n';
+}
+
+//文件payload
+function getfieldHead(field, filename) {
+    var fileFieldHead = 'Content-Disposition: form-data; name="files[]"; filename="' + filename + '"\r\n' + 'Content-Type: ' + getMime(filename) + '\r\n\r\n\r\n';
+    return fileFieldHead;
+}
+//获取Mime
+function getMime(filename) {
+    var mimes = {
+        '.png': 'image/png',
+        '.gif': 'image/gif',
+        '.jpg': 'image/jpeg',
+        '.jpeg': 'image/jpeg',
+        '.js': 'appliction/json',
+        '.torrent': 'application/octet-stream'
+    };
+    var ext = path.extname(filename);
+    var mime = mimes[ext];
+    mime = !!mime ? mime : 'application/octet-stream';
+    return mime;
+}
+
+//获取边界检查随机串
+function getBoundary() {
+    var max = 9007199254740992;
+    var dec = Math.random() * max;
+    var hex = dec.toString(36);
+    var boundary = hex;
+    return boundary;
+}
+
+//获取boundary
+function getBoundaryBorder(boundary) {
+    return '--' + boundary + '\r\n';
+}
+
+//字段格式化
+function fieldPayload(opts) {
+    var payload = [];
+    for (var id in opts.field) {
+        payload.push(getfield(id, opts.field[id]));
+    }
+
+    payload.push("");
+    return payload.join(getBoundaryBorder(opts.boundary));
+}
+
+// function getMytaskById(cookie, taskID) {
+//     superagent.get('http://mx5.maxthon.net/index.php?m=task&f=edit&taskID=' + taskID)
+//         .set("Cookie", cookie[2])
+//         .end((err, response) => {
+
+//             if (err) console.log(err)
+//             let $ = cheerio.load(response.text);
+
+//             let dataform = $('#dataform');
+//             var options = {
+//                 'url' : 'http://mx5.maxthon.net/index.php?m=task&f=edit&taskID=' + taskID,
+//                 "file":"",
+//                 "filename": "",
+//                 "param":"file",
+//                 'cookie':cookie,
+//                 "field":{ //其余post字段
+//                     'name' : dataform.find('#name').val(),
+//                     'desc' : dataform.find('#desc').val(),
+//                     'comment' : dataform.find('#comment').val(),
+//                     // 'files[]' : '',
+//                     'labels[]' : '', 
+//                     'consumed' : '1',
+//                     'project' : dataform.find('#project').val(),
+//                     'module' : dataform.find('#module').val(),
+//                     'story' : '',
+//                     'assignedTo' : dataform.find('#assignedTo').val(),
+//                     'type' : dataform.find('#type').val(),
+//                     'status' : dataform.find('#status').val(),
+//                     'pri' : dataform.find('#pri').val(),
+//                     'mailto[]' : '',
+//                     'estStarted' : dataform.find('#estStarted').val(),
+//                     'realStarted' : dataform.find('#realStarted').val(),
+//                     'deadline' : dataform.find('#deadline').val(),
+//                     'estimate' : dataform.find('#estimate').val(),
+//                     'left' : dataform.find('#left').val(),
+//                     'finishedBy' : dataform.find('#finishedBy').val(),
+//                     'finishedDate' : dataform.find('#finishedDate').val(),
+//                     'canceledBy' :dataform.find('#canceledBy').val(),
+//                     'canceledDate' : dataform.find('#canceledDate').val(),
+//                     'closedBy' : dataform.find('#closedBy').val(),
+//                     'closedReason' : dataform.find('#closedReason').val(),
+//                     'closedDate' : dataform.find('#closedDate').val()
+//                 },
+//                 "boundary":"---------------------------WebKitFormBoundary"+getBoundary()
+//             };
+
+//             postRequest(options, function(statusCode) {
+
+//             });
+//         });
+// }
